a17a4eec02c7ab7a875d15e612dca7a1ce9ac36f
taijulsir <princetaijul123@gmail.com>
2023-10-04 02:05:12 +0600

    update firebase

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1gqZmTPMc7vNOMatnTJZHXL950vdvNfY",
-  authDomain: "auth-react-dragon-news.firebaseapp.com",
-  projectId: "auth-react-dragon-news",
-  storageBucket: "auth-react-dragon-news.appspot.com",
-  messagingSenderId: "165167596544",
-  appId: "1:165167596544:web:1204c306dcd848f1a844e5"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
